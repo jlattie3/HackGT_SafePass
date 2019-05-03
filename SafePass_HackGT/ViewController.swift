ea9c9ee1da2562518ae9c3492b6879b7b13e9ff1
Jacob Lattie <jhlattie@gatech.edu>
2019-05-03 17:53:42 -0400

    Initial Commit

---
+++ SafePass_HackGT/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SafePass_HackGT
+//
+//  Created by Jacob Lattie on 5/3/19.
+//  Copyright © 2019 Jacob Lattie. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ SafePass_HackGTTests/SafePass_HackGTTests.swift
@@ -0,0 +1,34 @@
+//
+//  SafePass_HackGTTests.swift
+//  SafePass_HackGTTests
+//
+//  Created by Jacob Lattie on 5/3/19.
+//  Copyright © 2019 Jacob Lattie. All rights reserved.
+//
+
+import XCTest
+@testable import SafePass_HackGT
+
+class SafePass_HackGTTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
